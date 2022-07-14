2e82c07d089118a436d38774da482b81f08cd088
john adegoke <johnshay77@gmail.com>
2022-07-14 18:34:30 +0000

    challenge 1

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,14 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let sum =0;
+  for(i=0; i<arrayOfNumbers.length; i++){
+    sum = sum + arrayOfNumbers[i]
+  }
+    return sum
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
+
 
 /**
  * Challenge - 2
@@ -28,9 +33,16 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let noOfEvenNumbers=0
+  for(i=0; i<arrayOfNumbers.lenght; i++){
+    if(arrayOfNumbers[i]%2===0){
+      noOfEvenNumbers++
+    }
+  }
+    return noOfEvenNumbers ;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+ assignment.countEvenNumbers = countEvenNumbers;
+
 
 /**
  * Challenge - 3
